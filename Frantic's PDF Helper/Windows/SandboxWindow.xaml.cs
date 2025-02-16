69a51a0c4c5ed921ca881c1d5ed6599079adb82d
FranticDreamer <edwardgmgmd@gmail.com>
2025-02-16 06:52:45 +0300

    Add Basic Drawing and Erasing Functionality
    Freehand, Line, Rectangle and Ellipse.
    TODO: Implement Shape Fill
    TODO: Implement Undo, Redo
    TODO: Implement Erase All

--- Frantic's PDF Helper/Windows/SandboxWindow.xaml.cs
+++ Frantic's PDF Helper/Windows/SandboxWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
@@ -13,7 +13,27 @@ namespace Frantics_PDF_Helper.Windows
 	/// </summary>
 	public partial class SandboxWindow : Window
 	{
-		private Point drawBrushPoint;
+		public enum DrawMode
+		{
+			Freehand,
+			Line,
+			Rectangle,
+			Ellipse,
+			Erase
+		}
+
+		private Point drawBrushStartPoint;
+		private Point drawBrushCurrentPoint;
+
+		private Brush drawBrush = Brushes.Black;
+		private double drawBrushThickness = 2.0;
+		private DrawMode drawMode = DrawMode.Rectangle;
+
+		private bool isDrawing = false;
+		private Shape? currentDrawnShape = null;
+		private double currentDrawnShapeWidth = 0;
+		private double currentDrawnShapeHeight = 0;
+		private bool shouldFillShape = false;
 
 		public SandboxWindow(ImageSource ? src = null)
 		{
@@ -24,7 +44,7 @@ namespace Frantics_PDF_Helper.Windows
 			{
 				Image img = new Image();
 				img.Source = src;
-				Content = img;
+				drawCanvas.Children.Add(img);
 			}
 
 		}
@@ -32,15 +52,173 @@ namespace Frantics_PDF_Helper.Windows
 		private void DrawCanvas_MouseDown(object sender, MouseButtonEventArgs e)
 		{
 			if (e.ButtonState == MouseButtonState.Pressed)
-				drawBrushPoint = e.GetPosition(this);
+			{
+				drawBrushStartPoint = e.GetPosition(this);
+				drawBrushCurrentPoint = drawBrushStartPoint;
+				
+				switch (drawMode)
+				{
+					case DrawMode.Freehand:
+						currentDrawnShape = new Polyline();
+						((Polyline)currentDrawnShape).Stroke = drawBrush;
+						((Polyline)currentDrawnShape).StrokeThickness = drawBrushThickness;
+						((Polyline)currentDrawnShape).Points.Add(drawBrushCurrentPoint);
+						drawCanvas.Children.Add(currentDrawnShape);
+						break;
+					case DrawMode.Line:
+						currentDrawnShape = new Line();
+						((Line)currentDrawnShape).Stroke = drawBrush;
+						((Line)currentDrawnShape).StrokeThickness = drawBrushThickness;
+						((Line)currentDrawnShape).X1 = drawBrushCurrentPoint.X;
+						((Line)currentDrawnShape).Y1 = drawBrushCurrentPoint.Y;
+						((Line)currentDrawnShape).X2 = drawBrushCurrentPoint.X;
+						((Line)currentDrawnShape).Y2 = drawBrushCurrentPoint.Y;
+						drawCanvas.Children.Add(currentDrawnShape);
+						break;
+					case DrawMode.Rectangle:
+						if (shouldFillShape)
+						{
+							currentDrawnShape = new Rectangle();
+							((Rectangle)currentDrawnShape).Stroke = drawBrush;
+							((Rectangle)currentDrawnShape).StrokeThickness = drawBrushThickness;
+							((Rectangle)currentDrawnShape).Fill = Brushes.Transparent;
+							((Rectangle)currentDrawnShape).Width = 0;
+							((Rectangle)currentDrawnShape).Height = 0;
+							Canvas.SetLeft(currentDrawnShape, drawBrushCurrentPoint.X);
+							Canvas.SetTop(currentDrawnShape, drawBrushCurrentPoint.Y);
+						}
+						else
+						{
+							// To make ONLY the border hit testable, we need to create a rectangle with a border
+							// We can do that with a Polyline
+							currentDrawnShape = new Polyline();
+							((Polyline)currentDrawnShape).Stroke = drawBrush;
+							((Polyline)currentDrawnShape).StrokeThickness = drawBrushThickness;
+
+							// To save performance, we should add the points first
+							// Then modify them acoordingly later, instead of clearing and adding new points.
+							for (int i = 0; i < 5; i++) // Four corners, and an extra to close the shape
+							{
+								((Polyline)currentDrawnShape).Points.Add(new Point());
+							}
+						}
+						drawCanvas.Children.Add(currentDrawnShape);
+						break;
+					case DrawMode.Ellipse:
+						currentDrawnShape = new Ellipse();
+						((Ellipse)currentDrawnShape).Stroke = drawBrush;
+						((Ellipse)currentDrawnShape).StrokeThickness = drawBrushThickness;
+						((Ellipse)currentDrawnShape).Fill = Brushes.Transparent;
+						((Ellipse)currentDrawnShape).Width = 0;
+						((Ellipse)currentDrawnShape).Height = 0;
+						Canvas.SetLeft(currentDrawnShape, drawBrushCurrentPoint.X);
+						Canvas.SetTop(currentDrawnShape, drawBrushCurrentPoint.Y);
+						drawCanvas.Children.Add(currentDrawnShape);
+						break;
+					case DrawMode.Erase:
+						currentDrawnShape = new Line(); // Just to pass the null check
+						break;
+				}
+
+				isDrawing = true;
+			}
 		}
 
 		private void DrawCanvas_MouseMove(object sender, MouseEventArgs e)
 		{
-			if (e.LeftButton == MouseButtonState.Pressed)
+			if (e.LeftButton == MouseButtonState.Pressed && isDrawing && currentDrawnShape != null)
 			{
-				drawBrushPoint = e.GetPosition(this);
+				drawBrushCurrentPoint = e.GetPosition(this);
+
+				switch (drawMode)
+				{
+					case DrawMode.Freehand:
+						((Polyline)currentDrawnShape).Points.Add(drawBrushCurrentPoint);
+						break;
+					case DrawMode.Line:
+						((Line)currentDrawnShape).X2 = drawBrushCurrentPoint.X;
+						((Line)currentDrawnShape).Y2 = drawBrushCurrentPoint.Y;
+						break;
+					case DrawMode.Rectangle:
+						if (shouldFillShape)
+						{
+							//((Rectangle)currentDrawnShape).Fill = drawBrush;
+							currentDrawnShapeWidth = Math.Abs(drawBrushCurrentPoint.X - drawBrushStartPoint.X);
+							currentDrawnShapeHeight = Math.Abs(drawBrushCurrentPoint.Y - drawBrushStartPoint.Y);
+							Canvas.SetLeft(currentDrawnShape, Math.Min(drawBrushStartPoint.X, drawBrushCurrentPoint.X));
+							Canvas.SetTop(currentDrawnShape, Math.Min(drawBrushStartPoint.Y, drawBrushCurrentPoint.Y));
+							((Rectangle)currentDrawnShape).Width = currentDrawnShapeWidth;
+							((Rectangle)currentDrawnShape).Height = currentDrawnShapeHeight;
+						}
+						else
+						{
+							// Let's set the points of the polyline
+							((Polyline)currentDrawnShape).Points[0] = new Point(drawBrushStartPoint.X, drawBrushStartPoint.Y);
+							((Polyline)currentDrawnShape).Points[1] = new Point(drawBrushCurrentPoint.X, drawBrushStartPoint.Y);
+							((Polyline)currentDrawnShape).Points[2] = new Point(drawBrushCurrentPoint.X, drawBrushCurrentPoint.Y);
+							((Polyline)currentDrawnShape).Points[3] = new Point(drawBrushStartPoint.X, drawBrushCurrentPoint.Y);
+							((Polyline)currentDrawnShape).Points[4] = new Point(drawBrushStartPoint.X, drawBrushStartPoint.Y);
+						}
+						break;
+					case DrawMode.Ellipse:
+						currentDrawnShapeWidth = Math.Abs(drawBrushCurrentPoint.X - drawBrushStartPoint.X);
+						currentDrawnShapeHeight = Math.Abs(drawBrushCurrentPoint.Y - drawBrushStartPoint.Y);
+						Canvas.SetLeft(currentDrawnShape, Math.Min(drawBrushStartPoint.X, drawBrushCurrentPoint.X));
+						Canvas.SetTop(currentDrawnShape, Math.Min(drawBrushStartPoint.Y, drawBrushCurrentPoint.Y));
+						((Ellipse)currentDrawnShape).Width = currentDrawnShapeWidth;
+						((Ellipse)currentDrawnShape).Height = currentDrawnShapeHeight;
+						break;
+					case DrawMode.Erase:
+						// Let's remove all shapes that are under the cursor
+						foreach (var child in drawCanvas.Children)
+						{
+							// You can filter for specific shapes if needed, like Polylines, Rectangles, Ellipses, etc.
+							if (child is Shape shape)
+							{
+								if (shape.IsMouseOver)
+								{
+									drawCanvas.Children.Remove(shape);
+									break;
+								}
+							}
+						}
+						break;
+				}
 			}
+		}
+
+		private void DrawCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ButtonState == MouseButtonState.Released)
+			{
+				isDrawing = false;
+				currentDrawnShape = null;
+			}
+		}
+
+		private void EraserButton_Click(object sender, RoutedEventArgs e)
+		{
+			drawMode = DrawMode.Erase;
+		}
+
+		private void FreehandButton_Click(object sender, RoutedEventArgs e)
+		{
+			drawMode = DrawMode.Freehand;
+		}
+
+		private void LineButton_Click(object sender, RoutedEventArgs e)
+		{
+			drawMode = DrawMode.Line;
+		}
+
+		private void RectangleButton_Click(object sender, RoutedEventArgs e)
+		{
+			drawMode = DrawMode.Rectangle;
+		}
+
+		private void EllipseButton_Click(object sender, RoutedEventArgs e)
+		{
+			drawMode = DrawMode.Ellipse;
 		}
 	}
 }
